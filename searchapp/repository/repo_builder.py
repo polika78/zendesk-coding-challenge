31523a9eac44b83ead82f6b3838799a8331dea45
James Lee <james.lee@rea-group.com>
2021-06-23 22:08:43 +1000

    Fix bug with multiple records

--- searchapp/repository/repo_builder.py
+++ searchapp/repository/repo_builder.py
@@ -1,5 +1,5 @@
 from itertools import groupby
-from typing import Callable, Dict, Generator, Iterable, List 
+from typing import Callable, Dict, Generator, List, Tuple
 
 class RepoBuilder:
 
@@ -11,23 +11,28 @@ class RepoBuilder:
 
     @staticmethod
     def build_search_index(term: str, data: Dict[str, object]) -> Dict[str, List[str]]:
-        def generate_map_data_with_term_value(term: str, id: str, ids: Iterable[str], data: object) -> Generator:
+        def generate_map_data_with_term_value(result_map: List[Tuple[str, str]], term: str, id: str, ids: List[str], data: dict) -> List[Tuple[str, str]]:
             if isinstance(getattr(data[id], term), list):
-                for value in getattr(data[id], term):
-                    yield (value, id)
+                value_map = [(str(value).lower(), id) for value in getattr(data[id], term)]
             else:
-                yield (getattr(data[id], term), id)
+                value_map = [(getattr(data[id], term), id)]
 
-            next_id = next(ids, None)
-            if next_id:
-                return generate_map_data_with_term_value(term, next_id, ids, data)
+            new_result_map = [
+                *result_map,
+                *value_map
+            ]
+            
+            if ids:
+                next_id, *rest_ids = ids
+                return generate_map_data_with_term_value(new_result_map, term, next_id, rest_ids, data)
+
+            return new_result_map
 
         indexing = {}
-        ids = iter(data)
-        id = next(ids)
-        map_data_with_term_value = generate_map_data_with_term_value(term, id, ids, data)
+        id, *ids = iter(data)
+        map_data_with_term_value = generate_map_data_with_term_value([], term, id, ids, data)
 
-        for value, groupby_term_value in groupby(map_data_with_term_value, lambda x: x[0]):
+        for value, groupby_term_value in groupby(sorted(map_data_with_term_value), lambda x: x[0]):
             indexing[str(value).lower()] = list(map(lambda x: x[1], list(groupby_term_value)))
 
         return indexing
--- tests/searchapp/repository/test_repo_builder.py
+++ tests/searchapp/repository/test_repo_builder.py
@@ -38,6 +38,44 @@ class TestRepoBuilder:
             )
         }
 
+    @pytest.fixture
+    def input_multiple_records(self):
+        return [
+            {
+                "_id": 1,
+                "name": "James",
+                "tags": ["Foo", "bar"],
+                "foreign_id": 100,
+                "active": True
+            },
+            {
+                "_id": 5,
+                "name": "Tom",
+                "tags": ["Bar", "foo"],
+                "foreign_id": 200,
+                "active": True
+            }
+        ]
+
+    @pytest.fixture
+    def input_multiple_data(self):
+        return {
+            "1": TestModel(
+                _id=1,
+                name="James",
+                tags=["Foo", "bar"],
+                foreign_id=100,
+                active=True
+            ),
+            "5": TestModel(
+                _id=5,
+                name="Tom",
+                tags=["Bar", "foo"],
+                foreign_id=200,
+                active=True
+            )
+        }
+
     def test_given_records_and_model_func_build_data_returns_id_indexed_data(self, input_records):
         def create_test_model(raw):
             return TestModel(**raw)
@@ -74,4 +112,51 @@ class TestRepoBuilder:
         assert indexed_map == {
             'foo': ["1"],
             'bar': ["1"]
+        }
+
+    def test_given_multiple_records_and_model_func_build_data_returns_id_indexed_data(self, input_multiple_records):
+        def create_test_model(raw):
+            return TestModel(**raw)
+
+        data = RepoBuilder.build_data(input_multiple_records, create_test_model)
+
+        assert data == {
+            "1": TestModel(
+                _id=1,
+                name="James",
+                tags=["Foo", "bar"],
+                foreign_id=100,
+                active=True
+            ),
+            "5": TestModel(
+                _id=5,
+                name="Tom",
+                tags=["Bar", "Foo"],
+                foreign_id=100,
+                active=True
+            )
+        }
+
+    def test_given_multiple_data_and_string_type_term_build_search_index_returns_term_indexed_map(self, input_multiple_data):
+        indexed_map = RepoBuilder.build_search_index('name', input_multiple_data)
+
+        assert indexed_map == {
+            'james': ["1"],
+            'tom': ["5"]
+        }
+
+    def test_given_multiple_data_and_int_type_term_build_search_index_returns_term_indexed_map(self, input_multiple_data):
+        indexed_map = RepoBuilder.build_search_index('foreign_id', input_multiple_data)
+
+        assert indexed_map == {
+            "100": ["1"],
+            "200": ["5"]
+        }
+
+    def test_given_multiple_data_and_array_type_term_build_search_index_returns_indexed_map(self, input_multiple_data):
+        indexed_map = RepoBuilder.build_search_index('tags', input_multiple_data)
+
+        assert indexed_map == {
+            'foo': ["1", "5"],
+            'bar': ["1", "5"]
         }
