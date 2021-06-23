a8c619e9bae6fc72dc020ad528ec2d12d0ce6b89
James Lee <james.lee@rea-group.com>
2021-06-23 21:01:43 +1000

    Add build_search_index

--- searchapp/repository/repo_builder.py
+++ searchapp/repository/repo_builder.py
@@ -1,4 +1,5 @@
-from typing import Callable, Dict, List, Union
+from itertools import groupby
+from typing import Callable, Dict, List
 
 class RepoBuilder:
 
@@ -7,3 +8,13 @@ class RepoBuilder:
         return dict(
             [(str(record['_id']).lower(), model_func(record)) for record in records]
         )
+
+    @staticmethod
+    def build_search_index(term: str, data: Dict[str, object]) -> Dict[str, List[str]]:
+        indexing = {}
+        map_data_with_term_value = [(getattr(data[id], term), id) for id in iter(data)]
+
+        for value, groupby_term_value in groupby(map_data_with_term_value, lambda x: x[0]):
+            indexing[str(value).lower()] = list(map(lambda x: x[1], list(groupby_term_value)))
+
+        return indexing
--- tests/searchapp/repository/test_repo_builder.py
+++ tests/searchapp/repository/test_repo_builder.py
@@ -26,6 +26,18 @@ class TestRepoBuilder:
             }
         ]
 
+    @pytest.fixture
+    def input_data(self):
+        return {
+            "1": TestModel(
+                _id=1,
+                name="James",
+                tags=["Foo", "bar"],
+                foreign_id=100,
+                active=True
+            )
+        }
+
     def test_given_records_and_model_func_build_data_returns_id_indexed_data(self, input_records):
         def create_test_model(raw):
             return TestModel(**raw)
@@ -40,4 +52,11 @@ class TestRepoBuilder:
                 foreign_id=100,
                 active=True
             )
+        }
+
+    def test_given_data_and_string_type_term_build_search_index_returns_term_indexed_map(self, input_data):
+        indexed_map = RepoBuilder.build_search_index('name', input_data)
+
+        assert indexed_map == {
+            'james': ["1"]
         }
