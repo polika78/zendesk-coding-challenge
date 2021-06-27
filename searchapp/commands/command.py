6c9530e66872422a4523b3d4a52157a7cb0cb354
James Lee <james.lee@rea-group.com>
2021-06-27 22:45:33 +1000

    Add run method in Command class

--- searchapp/commands/command.py
+++ searchapp/commands/command.py
@@ -1,5 +1,11 @@
+from typing import List
+
+from searchapp.models.command_result import CommandResult
 from searchapp.search_service import SearchService
 class Command:
     
     def __init__(self, search_service: SearchService) -> None:
         self.search_service = search_service
+
+    def run(self, args: List[str]) -> CommandResult:
+        raise NotImplementedError()
